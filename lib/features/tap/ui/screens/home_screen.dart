422dd7e5b77ffab1d70f3b177dc0cea5c9500ad0
aliatakishiyev <aliatakishiyev2006@gmail.com>
2026-03-11 16:13:58 +0300

    refactor: color change implemented to home screen

--- lib/features/tap/ui/screens/home_screen.dart
+++ lib/features/tap/ui/screens/home_screen.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:tapsoft/features/tap/services/color_generator.dart';
 import 'package:tapsoft/features/tap/ui/widgets/custom_app_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color? currentColor;
+
+  @override
+  void initState() {
+    super.initState();
+    currentColor = ColorGenerator.generateRandomColor();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.from(alpha: 255, red: 255, green: 255, blue: 255),
+      backgroundColor: currentColor,
       body: SafeArea(
         child: Column(
           children: [
             CustomAppBar(),
             Expanded(
               child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
                 onTap: () {
-                  
+                  setState(() {
+                    currentColor = ColorGenerator.generateRandomColor();
+                  });
                 },
-                child: Center(
-                  child: Text("Hello there"),
-                ),
+                child: Center(child: Text("Hello there",style: TextStyle(fontSize: 32),)),
               ),
             ),
           ],
